a6ee596b9b22d8495accf765de16138e79a21ff8
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2022-08-24 23:34:43 -0300

    Add first commit in Repo

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
-	
-
 	"github.com/gofiber/fiber/v2"
 )
 
+// creamos un struct para trabajar
+type Usuario struct {
+	Nombre   string
+	Apellido string
+}
+
+// funcion
+func handleUsuario(c *fiber.Ctx) error {
+	usuario := Usuario{
+		Nombre:   "Mauro",
+		Apellido: "Bogado",
+	}
+	return c.Status(fiber.StatusOK).JSON(usuario)
+}
+
+// funcion de creacion
+func CrearUsuario(c *fiber.Ctx) error {
+	//usamos la varible para asignar
+	usuario := Usuario{}
+	//parseamos los valores
+	if err := c.BodyParser(&usuario); err != nil {
+		return err
+	}
+	return c.Status(fiber.StatusOK).JSON(usuario)
+}
 func main() {
-	
+
 	//creacion de la var
 	appli := fiber.New()
-	
+
 	//usamos metodo get para escuhar
-	appli.Get("/user", func(a *fiber.Ctx) error {
-		return a.SendString("Hola Usuario de API")
+	appli.Get("/", func(a *fiber.Ctx) error {
+		return a.SendString("Hola Usuario de API en Fiber")
 	})
+	appli.Get("/usuario", handleUsuario)
+	//usamos el metodo post
+	appli.Post("/usuarios", CrearUsuario)
+
 	//puerto en donde va a escuchar
 	appli.Listen(":3000")
 }
